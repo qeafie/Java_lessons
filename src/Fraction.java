63d9e09e284f4fbab78a05783ddc9a81fd85baf9
qeafie <qeafie@yandex.ru>
2021-10-03 19:12:36 +0400

    Добавлен новый класс Fraction представляющий обыкновенные дроби и методы для работы с ними

---
+++ src/Fraction.java
@@ -0,0 +1,15 @@
+
+public class Fraction {
+    
+    int numerator;
+    int denominator;
+    
+    public Fraction(int numerator, int denominator){
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+    
+    public String toString(){
+        return numerator + "/" + denominator; 
+    }
+}
