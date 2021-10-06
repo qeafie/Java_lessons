c78b9df50476ad9488f37ddb7ef68d6dcd065885
qeafie <qeafie@yandex.ru>
2021-10-06 21:14:41 +0400

    Добавлены конструкторы и методы для класса Polyline

--- src/Polyline.java
+++ src/Polyline.java
@@ -1,27 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Polyline {
-    Point [] polyline;
-    
+    List<Point> polyline;
+
+    public Polyline(){
+        polyline =  new ArrayList<Point>();
+    }
+
     public Polyline(Point... polyline){
-        this.polyline = polyline;
+        this();
+        this.polyline.addAll(List.of(polyline));
     }
-   
-    
-    public Line getLine (){
-        
-        Point p1  = polyline[0];
-        Point p2  = polyline[polyline.length-1];
-        
-        return new Line(p1,p2);
+
+    public void addPoint(Point point){
+        polyline.add(point);
     }
+    public void addPoint(Point... points){
+        polyline.addAll(List.of(points));
+    }
+
+    public int getDistance(){
+        int result = 0;
+
+        for (int i = 1; i < polyline.size(); i++){
+            result += new Line(polyline.get(i-1),polyline.get(i)).getDistance();
+        }
+        return result;
+    }
+
     public  String toString(){
        String temp = "line : ";
        
        for(Point p: polyline){
            temp+= p; 
        }
-        
-       
       return temp;
     }
     
